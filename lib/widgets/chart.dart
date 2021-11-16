9b5efbe1072a48918400dbe473512898ce3bcfd5
MiNHAZ-33 <miminhaz33@gmail.com>
2021-11-17 00:05:21 +0600

    Chart skeleton is added

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,36 @@
+import 'package:expense_track/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      double totalSum = 0.0;
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+      return {'Day': DateFormat.E(weekDay), 'amount': 9.99};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      margin: EdgeInsets.all(10),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
